d5213fe5c7e5ad48492ae9d9440e406043211ed7
TheFallenCol <germansilva1944@hotmail.com>
2021-03-26 17:21:59 -0500

    Repository Customer created

---
+++ Angular-Udemy-First/DataAccess/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+using Example.Models;
+using Example.Repositories;
+
+namespace DataAccess
+{
+    public class CustomerRepository: Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public IEnumerable<Customer> CustomerPagedList(int page, int rows)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@page", page);
+            parameters.Add("rows", rows);
+            
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Customer>(
+                    "dbo.CustomerPagedList",
+                    parameters,
+                    commandType: System.Data.CommandType.StoredProcedure
+                );
+            }
+        }
+    }
+}
---
+++ Angular-Udemy-First/Example.Repositories/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Example.Models;
+
+namespace Example.Repositories
+{
+    public interface ICustomerRepository : IRepository<Customer>
+    {
+        IEnumerable<Customer> CustomerPagedList(int page, int rows);
+    }
+}
