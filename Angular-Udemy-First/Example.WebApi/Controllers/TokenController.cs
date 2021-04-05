1464a8b8bfdd8017b198ccf5dc75bce5007412c3
TheFallenCol <germansilva1944@hotmail.com>
2021-04-05 15:22:18 -0500

    Create token controller

---
+++ Angular-Udemy-First/Example.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Example.Models;
+using Example.UnitOfWork;
+using Example.WebApi.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly ITokenProvider _tokenProvider;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TokenController(ITokenProvider tokenProvider, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _tokenProvider = tokenProvider;
+        }
+
+        [HttpPost]
+        public JsonWebToken Post([FromBody]User userLogin)
+        {
+            var user = _unitOfWork.User.ValidateUser(userLogin.Email, userLogin.Password);
+            
+            if (user == null)
+                throw new UnauthorizedAccessException();
+
+            return new JsonWebToken()
+            {
+                AccessToken = _tokenProvider.CreateToke(user,DateTime.UtcNow.AddHours(8)),
+                ExpiresIn = 480 //Minutos expiracion token
+            };
+        }
+    }
+}
