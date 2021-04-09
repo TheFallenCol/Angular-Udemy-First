3914244310bfe3da8d13bff9f2fc324482e4966d
TheFallenCol <germansilva1944@hotmail.com>
2021-04-09 17:51:20 -0500

    Create supplier controler

---
+++ Angular-Udemy-First/Example.WebApi/Controllers/SupplierController.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Permissions;
+using Example.Models;
+using Example.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SupplierController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SupplierController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unitOfWork.Supplier.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("GetPaginatedSupplier/{page:int}/{rows:int}")]
+        public IActionResult GetPaginatedSupplier(int page, int rows)
+        {
+            return Ok(_unitOfWork.Supplier.SupplierPagedList(page, rows));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Supplier supplier)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            return Ok(_unitOfWork.Supplier.Insert(supplier));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Supplier supplier)
+        {
+            if (ModelState.IsValid && _unitOfWork.Supplier.Update(supplier))
+            {
+                return Ok(new {Message = "The supplier is updated"});
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody]Supplier supplier)
+        {
+            if (supplier.Id > 0)
+                return Ok(_unitOfWork.Supplier.Delete(supplier));
+
+            return BadRequest();
+        }
+
+    }
+}
