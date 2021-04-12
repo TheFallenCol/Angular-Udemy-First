36627f39b57e86a492082b1ea7ba656a0c870deb
TheFallenCol <germansilva1944@hotmail.com>
2021-04-12 17:03:40 -0500

    Routing to homeComponent instead homeModule

--- UIAngular-Example/src/app/app-routing.module.ts
+++ UIAngular-Example/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { HomeComponent } from './home/home/home.component';
 import { HomeModule } from './home/home.module';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-   { path:'', component:HomeModule },
-   { path:'**', component:HomeModule }
+   { path:'', component:HomeComponent },
+   { path:'**', component:HomeComponent }
 ];
 
 @NgModule({
