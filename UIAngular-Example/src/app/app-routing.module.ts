8b6b18584015f6202b10459f0a6d6d234c757176
TheFallenCol <germansilva1944@hotmail.com>
2021-04-12 16:56:59 -0500

    Modified routes with components

--- UIAngular-Example/src/app/app-routing.module.ts
+++ UIAngular-Example/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { HomeModule } from './home/home.module';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-   { path:'', component:NavBarComponent },
-   { path:'**', component:NavBarComponent }
+   { path:'', component:HomeModule },
+   { path:'**', component:HomeModule }
 ];
 
 @NgModule({
