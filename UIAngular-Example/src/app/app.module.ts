febacd00e8270e8ff67d8b615eaab01e2dd12066
TheFallenCol <germansilva1944@hotmail.com>
2021-04-15 17:33:59 -0500

    Remove module, will be charge under demand

--- UIAngular-Example/src/app/app.module.ts
+++ UIAngular-Example/src/app/app.module.ts
@@ -1,12 +1,10 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { CustomerModule } from './customer/customer.module';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +14,7 @@ import { HomeModule } from './home/home.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule,
-    CustomerModule,
-    HomeModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
