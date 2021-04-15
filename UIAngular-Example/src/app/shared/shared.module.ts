852558a8f6ea5cc54f447b6b2a9a37e6e759b808
TheFallenCol <germansilva1944@hotmail.com>
2021-04-15 17:40:29 -0500

    Add not found component

--- UIAngular-Example/src/app/shared/shared.module.ts
+++ UIAngular-Example/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-
-
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    NotFoundComponent
   ],
-  exports:[
-    NavBarComponent
+  exports: [
+    NavBarComponent,
+    NotFoundComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
