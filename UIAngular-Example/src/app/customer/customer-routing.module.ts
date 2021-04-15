1511b6897e636f2efe75f35ee642af0ff60af1e0
TheFallenCol <germansilva1944@hotmail.com>
2021-04-15 17:35:01 -0500

    Add supplier example

--- UIAngular-Example/src/app/customer/customer-routing.module.ts
+++ UIAngular-Example/src/app/customer/customer-routing.module.ts
@@ -1,9 +1,15 @@
+import { SupplierComponent } from './supplier/supplier.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path:'', component:CustomerListComponent }
+  {
+    path:'', children:[
+      { path:'', component:CustomerListComponent },
+      { path:'supplier', component:SupplierComponent }
+    ]
+  }
 ];
 
 @NgModule({
