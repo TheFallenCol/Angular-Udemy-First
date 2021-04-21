03424717987dfe1135adf50c2b4eb9eedfdf8070
TheFallenCol <germansilva1944@hotmail.com>
2021-04-20 20:32:18 -0500

    Add template form validators

--- UIAngular-Example/src/app/login/login.component.ts
+++ UIAngular-Example/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginError : boolean = false;
+  loginErrorMessage : string = '';
+  loginFormGroup: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.loginFormGroup = fb.group({
+      email : ['', [Validators.required, Validators.email]],
+      password : ['', [Validators.required, Validators.minLength(2), Validators.maxLength(5)]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  login(submittedForm: FormGroup){
+    debugger;
+  }
+
+  get email(){
+    return this.loginFormGroup.get('email');
+  }
+
+  get password(){
+    return this.loginFormGroup.get('password');
+  }
 }
