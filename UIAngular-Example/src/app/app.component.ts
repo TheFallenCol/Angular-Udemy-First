c7f8c251b2047cf262031f5167f99466e313f86b
TheFallenCol <germansilva1944@hotmail.com>
2021-05-03 17:44:06 -0500

    Implements ngOnInit to check authentication status

--- UIAngular-Example/src/app/app.component.ts
+++ UIAngular-Example/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'UIAngular-Example';
   _displayLogin: boolean = false;
+
+  constructor(private authService: AuthService){
+  }
+
+  ngOnInit(): void {
+    this.authService.authStatus.subscribe(authStatus => {
+      const jwt = this.authService.getToken();
+      setTimeout(() => {
+        this._displayLogin = (this.authService.isValidToken(jwt));
+      }, 0);
+    });
+  }
 
   get displayMenu(){
     return this._displayLogin;
