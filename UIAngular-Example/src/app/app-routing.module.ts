623d04835f8d3257f888d1a1e77002781755a3a6
TheFallenCol <germansilva1944@hotmail.com>
2021-06-29 18:03:07 -0500

    Add logout path

--- UIAngular-Example/src/app/app-routing.module.ts
+++ UIAngular-Example/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LogoutComponent } from './logout/logout.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { NgModule } from '@angular/core';
@@ -7,6 +8,7 @@ const routes: Routes = [
    { path:'home', loadChildren: () => import ('./home/home.module').then(m => m.HomeModule) },
    { path:'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
    { path:'login', component:LoginComponent },
+   { path:'logout', component:LogoutComponent },
    { path:'', redirectTo:'/login', pathMatch:'full' },
    { path:'**', component:NotFoundComponent }
 ];
