90393253c0eb2b1516c4daa416ca282a58f5efb3
TheFallenCol <germansilva1944@hotmail.com>
2021-07-02 19:03:31 -0500

    Add canActive from guard in routing

--- UIAngular-Example/src/app/customer/customer-routing.module.ts
+++ UIAngular-Example/src/app/customer/customer-routing.module.ts
@@ -1,14 +1,20 @@
+import { Role } from './../auth/role.enum';
+import { AuthGuard } from './../auth/auth.guard';
 import { SupplierComponent } from './supplier/supplier.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 
 const routes: Routes = [
   {
     path:'', children:[
       { path:'', component:CustomerListComponent },
       { path:'supplier', component:SupplierComponent }
-    ]
+    ],
+    canActivate:[AuthGuard],
+    data: {
+      expectedRole : [Role.Admin, Role.AdminProduct]
+    }
   }
 ];
 
