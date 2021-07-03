109cf336eb73bf432fd3299e1b8cec571c277638
TheFallenCol <germansilva1944@hotmail.com>
2021-07-02 19:02:24 -0500

    Add guard provider

--- UIAngular-Example/src/app/app-routing.module.ts
+++ UIAngular-Example/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { LogoutComponent } from './logout/logout.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
@@ -5,8 +6,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-   { path:'home', loadChildren: () => import ('./home/home.module').then(m => m.HomeModule) },
-   { path:'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+   {
+     path:'home',
+     loadChildren: () => import ('./home/home.module').then(m => m.HomeModule),
+     canLoad: [AuthGuard]
+   },
+   {
+     path:'customer',
+     loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
+     canLoad: [AuthGuard]
+   },
    { path:'login', component:LoginComponent },
    { path:'logout', component:LogoutComponent },
    { path:'', redirectTo:'/login', pathMatch:'full' },
